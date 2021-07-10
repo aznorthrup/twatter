4cf96b0ce8e4ee63ec8cf59db9ff45fd8ff5f663
Bryce Northrup <aznorthrup@gmail.com>
2021-07-10 08:23:01 +0000

    added submitions page and pagification

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,7 +1,12 @@
 class PlacesController < ApplicationController
-
+    PLACES_PER_PAGE = 3
     def index
-        @places = Place.all
+        @page = params.fetch(:page, 0).to_i
+        @places = Place.offset(@page * PLACES_PER_PAGE).limit(PLACES_PER_PAGE)
+    end
+
+    def new
+        @place = Place.new
     end
 
 end
