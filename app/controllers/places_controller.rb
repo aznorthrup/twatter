820f3b67282bdb98e53351ee790e272697a28473
Bryce Northrup <aznorthrup@gmail.com>
2021-07-11 09:58:16 +0000

    Added full user function!

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -5,14 +5,18 @@ class PlacesController < ApplicationController
         @places = Place.offset(@page * PLACES_PER_PAGE).limit(PLACES_PER_PAGE)
     end
 
+    before_action :authenticate_user!, only: [:new, :create]
+
     def new
         @place = Place.new
     end
 
     def create
-        Place.create(place_params)
+        current_user.places.create(place_params)
         redirect_to root_path
     end
+
+    private
 
     def place_params
         params.require(:place).permit(:name, :description, :address)
